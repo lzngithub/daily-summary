0569451673ffe77f4d9fbec27398619c1ea8cda8
lzngithub <1763466626@qq.com>
2022-12-05 12:28:08 +0800

    feat: prettier

--- webpack/webpack-template/.prettierrc.js
+++ webpack/webpack-template/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   trailingComma: 'es5',
   tabWidth: 4,
-  semi: true, // 结尾加分号
+  semi: true, // 结尾加分号s
   singleQuote: true, // 使用单引号
-  endOfLine: 'lf',
+  '--end-of-line': 'lf'
 };
