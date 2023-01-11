36d483a4a5796a8fff0803f8a29643e3201a612d
lzngithub <1763466626@qq.com>
2023-01-11 23:22:36 +0800

    feat: 调整

--- 工具/echart-project/src/components/echarts/index.js
+++ 工具/echart-project/src/components/echarts/index.js
@@ -1,31 +1,25 @@
-// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
-import * as echarts from "echarts/core";
-// 引入柱状图图表，图表后缀都为 Chart
-import { BarChart } from "echarts/charts";
-// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
-import {
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  DatasetComponent,
-  TransformComponent,
-} from "echarts/components";
-// 标签自动布局、全局过渡动画等特性
-import { LabelLayout, UniversalTransition } from "echarts/features";
-// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
-import { CanvasRenderer } from "echarts/renderers";
+import echarts from "./charts";
+import { useEffect, useRef } from "react";
 
-// 注册必须的组件
-echarts.use([
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  DatasetComponent,
-  TransformComponent,
-  BarChart,
-  LabelLayout,
-  UniversalTransition,
-  CanvasRenderer,
-]);
+export const Chart = (props) => {
+  const { option } = props;
+  const chartRef = useRef(null);
+  const domRef = useRef(null);
 
-export default echarts;
+  useEffect(() => {
+    console.log("init");
+    chartRef.current = echarts.init(domRef.current);
+    chartRef.current.setOption(option);
+  }, [option]);
+
+  useEffect(() => {
+    if (domRef.current && chartRef.current) {
+      window.addEventListener("resize", chartRef.current.resize);
+    }
+    return () => {
+      window.removeEventListener("resize", chartRef.current.resize);
+    };
+  }, []);
+
+  return <div ref={domRef} style={{ height: "300px", width: "30%" }}></div>;
+};
