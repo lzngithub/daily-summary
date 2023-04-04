8113ec749e8d4c135e6d3ebfea9ae360a007dc50
lzngithub <1763466626@qq.com>
2023-04-04 15:07:13 +0800

    feat: 数组

--- 算法和数据结构/index.js
+++ 算法和数据结构/index.js
@@ -1,30 +1,29 @@
-var swapPairs = function (head, n) {
-  let ret =
-    (slow =
-    fast =
-      {
-        val: 0,
-        next: head,
-      });
-  while (n--) {
-    fast = fast.next;
+var generateMatrix = function (n) {
+  let nums = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  let count = 1;
+  let row;
+  let col;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    row = i;
+    col = i;
+    for (; col < n - 1 - i; col++) nums[row][col] = count++;
+    for (; row < n - 1 - i; row++) nums[row][col] = count++;
+    for (; col > i; col--) nums[row][col] = count++;
+    for (; row > i; row--) nums[row][col] = count++;
   }
-  while (fast.next) {
-    fast = fast.next;
-    slow = slow.next;
-  }
-  slow.next = slow.next.next;
-  return ret.next;
+  if (n % 2 === 1) nums[Math.floor(n / 2)][Math.floor(n / 2)] = count++;
+  return nums;
 };
 
-let head = {
-  val: 5,
-  next: null,
-};
+console.log(generateMatrix(3));
+// n = 3
+// 1 2 3 0 0  0 2
+// 8 9 4
+// 7 6 5      2 2
 
-const b = swapPairs(head, 1);
-let c = b;
-while (c) {
-  console.log(c.val);
-  c = c.next;
-}
+// n=4
+// 1  2  3  4  8(0, 0) ->         (0, n-1)
+// 12 13 14 5  8(1, 0) -> (1 n-2)
+// 11 16 15 6  8
+// 10 9  8  7  8(n-1, 0) ->        (n -1, n-1)
+// 8 8 88  8  8/
