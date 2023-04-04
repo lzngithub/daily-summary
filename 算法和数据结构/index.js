bd47e5a225fc84d4480e3e62650c78612bd80e35
lzngithub <1763466626@qq.com>
2023-04-04 18:08:25 +0800

    feat: 排序算法

--- 算法和数据结构/index.js
+++ 算法和数据结构/index.js
@@ -1,29 +1,20 @@
-var generateMatrix = function (n) {
-  let nums = new Array(n).fill(0).map(() => new Array(n).fill(0));
-  let count = 1;
-  let row;
-  let col;
-  for (let i = 0; i < Math.floor(n / 2); i++) {
-    row = i;
-    col = i;
-    for (; col < n - 1 - i; col++) nums[row][col] = count++;
-    for (; row < n - 1 - i; row++) nums[row][col] = count++;
-    for (; col > i; col--) nums[row][col] = count++;
-    for (; row > i; row--) nums[row][col] = count++;
+let nums = [7, 6, 9, 3, 1, 5, 2, 4, 10, 11];
+
+function mergeSort(nums) {
+  if (nums.length < 2) return nums;
+  var middle = Math.floor(nums.length / 2),
+    left = nums.slice(0, middle),
+    right = nums.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+
+  function merge(left, right) {
+    var result = [];
+    while (left.length > 0 && right.length > 0) {
+      let name = left[0] < right[0] ? left : right;
+      result.push(name.shift());
+    }
+    return result.concat(left, right);
   }
-  if (n % 2 === 1) nums[Math.floor(n / 2)][Math.floor(n / 2)] = count++;
-  return nums;
-};
+}
 
-console.log(generateMatrix(3));
-// n = 3
-// 1 2 3 0 0  0 2
-// 8 9 4
-// 7 6 5      2 2
-
-// n=4
-// 1  2  3  4  8(0, 0) ->         (0, n-1)
-// 12 13 14 5  8(1, 0) -> (1 n-2)
-// 11 16 15 6  8
-// 10 9  8  7  8(n-1, 0) ->        (n -1, n-1)
-// 8 8 88  8  8/
+console.log(mergeSort(nums));
