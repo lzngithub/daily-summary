1db8497db9b80a08f18a7223ca6c9f46c85e1beb
lzngithub <1763466626@qq.com>
2023-04-12 10:30:22 +0800

    feat: d3学习

--- 工具/d3-project/src/pages/d3Pratice/d3Tree.js
+++ 工具/d3-project/src/pages/d3Pratice/d3Tree.js
@@ -20,31 +20,59 @@ class D3Tree {
       .append("svg")
       .attr("width", 1280)
       .attr("height", 600);
-    const group = svg.append("g").attr("transform", "translate(640,50)");
+
+    //箭头(下半部分)
+    svg
+      .append("marker")
+      .attr("id", "markerOfDown")
+      .attr("markerUnits", "userSpaceOnUse")
+      .attr("viewBox", "0 -5 10 10") //坐标系的区域
+      .attr("refX", 50) //箭头坐标
+      .attr("refY", 0)
+      .attr("markerWidth", 10) //标识的大小
+      .attr("markerHeight", 10)
+      .attr("orient", "90") //绘制方向，可设定为：auto（自动确认方向）和 角度值
+      .attr("stroke-width", 2) //箭头宽度
+      .append("path")
+      .attr("d", "M0,-5L10,0L0,5") //箭头的路径
+      .attr("fill", "#63B4FF"); //箭头颜色
+
+    //箭头(上半部分)
+    svg
+      .append("marker")
+      .attr("id", "markerOfUp")
+      .attr("markerUnits", "userSpaceOnUse")
+      .attr("viewBox", "0 -5 10 10") //坐标系的区域
+      .attr("refX", -40) //箭头坐标
+      .attr("refY", 0)
+      .attr("markerWidth", 10) //标识的大小
+      .attr("markerHeight", 10)
+      .attr("orient", "90") //绘制方向，可设定为：auto（自动确认方向）和 角度值
+      .attr("stroke-width", 2) //箭头宽度
+      .append("path")
+      .attr("d", "M0,-5L10,0L0,5") //箭头的路径
+      .attr("fill", "#63B4FF"); //箭头颜色
+
+    const group = svg.append("g").attr("transform", "translate(640,300)");
 
     const dataSet = d3.hierarchy(this.data);
     console.log(dataSet);
     //创建树布局
-    const tree = d3.tree().size([600, 600]).nodeSize([150, 100]);
+    const tree = d3.tree().size([600, 600]).nodeSize([150, 130]);
     //所有的节点
     const nodes = tree(dataSet);
-    console.log(nodes);
-    console.log(nodes.descendants()); // 返回所有节点
 
     group
       .selectAll(".link")
       .data(nodes.links())
       .join("path")
       .attr("class", "link")
-      .attr(
-        "d",
-        d3
-          .linkHorizontal()
-          .x((d) => d.x)
-          .y((d) => d.y)
-      )
+      .attr("d", (d) => {
+        return this.drawLink(d);
+      })
       .attr("fill", "none")
-      .attr("stroke", "#5682ec");
+      .attr("stroke", "#5682ec")
+      .attr("marker-end", "url(#markerOfDown)");
 
     const nodeGroups = group
       .selectAll(".node")
@@ -72,6 +100,58 @@ class D3Tree {
       .attr("text-anchor", "middle")
       .attr("fill", "#fff")
       .text((d) => d.data.name);
+
+    //所有的节点
+    const nodesOfUp = tree(dataSet);
+    console.log(nodesOfUp.descendants());
+    nodesOfUp.descendants().forEach((node) => {
+      node.y = -node.y;
+    });
+    group
+      .selectAll(".linkUp")
+      .data(nodesOfUp.links())
+      .join("path")
+      .attr("class", "linkUp")
+      .attr("d", (d) => {
+        return this.drawLink(d);
+      })
+      .attr("fill", "none")
+      .attr("stroke", "#5682ec")
+      .attr("marker-end", "url(#markerOfUp)");
+
+    const nodeUpGroups = group
+      .selectAll(".nodeUp")
+      .data(nodesOfUp.descendants())
+      .join("g")
+      .attr("calss", (d) => {
+        return "nodeUp" + (d.children ? " nodeUp--internal" : " nodeUp--leaf");
+      })
+      .attr("transform", (d) => {
+        return `translate(${d.x},${d.y})`;
+      });
+
+    nodeUpGroups
+      .append("rect")
+      .attr("width", 100)
+      .attr("height", 50)
+      .attr("x", -50)
+      .attr("y", -25)
+      .attr("fill", "#7A9EFF");
+
+    nodeUpGroups
+      .append("text")
+      .attr("dy", ".33em")
+      .attr("font-size", "12px")
+      .attr("text-anchor", "middle")
+      .attr("fill", "#fff")
+      .text((d) => d.data.name);
+  }
+
+  // 直角连接线
+  drawLink({ source, target }) {
+    const halfDistance = (target.y - source.y) / 2;
+    const halfY = source.y + halfDistance;
+    return `M${source.x},${source.y} L${source.x},${halfY} ${target.x},${halfY} ${target.x},${target.y}`;
   }
 }
 
--- 工具/d3-project/src/pages/d3Pratice/data.js
+++ 工具/d3-project/src/pages/d3Pratice/data.js
@@ -2,8 +2,8 @@ export const data = {
   name: "中国",
   children: [
     { name: "北京" },
-    { name: "陕西", children: [{ name: "宝鸡" }, { name: "西安" }] },
+    { name: "陕西" },
     { name: "上海" },
-    { name: "浙江", children: [{ name: "杭州" }, { name: "温州" }] },
+    { name: "浙江" },
   ],
 };
