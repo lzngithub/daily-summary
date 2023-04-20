13eccaa7b3c4d9278af860551a36abfcb7585743
lzngithub <1763466626@qq.com>
2023-04-20 17:01:16 +0800

    feat: 算法

--- 算法和数据结构/index.js
+++ 算法和数据结构/index.js
@@ -1,20 +1,38 @@
 let nums = [7, 6, 9, 3, 1, 5, 2, 4, 10, 11];
 
-function mergeSort(nums) {
-  if (nums.length < 2) return nums;
-  var middle = Math.floor(nums.length / 2),
-    left = nums.slice(0, middle),
-    right = nums.slice(middle);
-  return merge(mergeSort(left), mergeSort(right));
+function heapSort(origin = []) {
+  let len = origin.length;
 
-  function merge(left, right) {
-    var result = [];
-    while (left.length > 0 && right.length > 0) {
-      let name = left[0] < right[0] ? left : right;
-      result.push(name.shift());
+  function buildMaxHeap(origin) {
+    for (let i = Math.floor(origin.length / 2); i > 0; i--) {
+      heapify(origin, i);
     }
-    return result.concat(left, right);
   }
+
+  function heapify(origin, i) {
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
+    let maxIndex = i;
+    if (left < len && origin[left] > origin[maxIndex]) {
+      maxIndex = left;
+    }
+    if (right < len && origin[right] > origin[maxIndex]) {
+      maxIndex = right;
+    }
+
+    if (maxIndex !== i) {
+      [origin[maxIndex], origin[i]] = [origin[i], origin[maxIndex]];
+      heapify(origin, maxIndex);
+    }
+  }
+
+  buildMaxHeap(origin);
+  for (let i = origin.length - 1; i > 0; i--) {
+    [origin[0], origin[i]] = [origin[i], origin[0]];
+    len--;
+    heapify(origin, 0);
+  }
+  return origin;
 }
 
-console.log(mergeSort(nums));
+console.log(heapSort(nums));
