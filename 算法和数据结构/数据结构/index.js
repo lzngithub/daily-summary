afc32e7323a4346f279620e8f08da6f290e74192
liangzn <1763466626@qq.com>
2024-01-06 22:57:23 +0800

    feat: http

--- 算法和数据结构/数据结构/index.js
+++ 算法和数据结构/数据结构/index.js
@@ -1,75 +1,27 @@
-// function levelOrder(root, queue = [], result = []) {
-//   if (root) queue.push(root);
-//   if (queue.length === 0) return [];
-//   let len = queue.length;
-//   result.push([]);
-//   for (let i = 0; i < len; i++) {
-//     let currentNode = queue.shift();
-//     result[result.length - 1].push(currentNode.value);
-//     if (currentNode.left) queue.push(currentNode.left);
-//     if (currentNode.right) queue.push(currentNode.right);
-//   }
-//   levelOrder(null, queue, result);
-//   return result;
-// }
+const linkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: null,
+    },
+  },
+};
 
-var levelOrder = (root) => {
-  const result = [];
-  const queue = [];
-  function handleTree() {
-    let len = queue.length; // 提前记录下队列的长度数据，每次递归是递归一层的节点
-    for (let i = 0; i < len; i++) {
-      let currentNode = queue.shift();
-      result.push(currentNode.value);
-      if (currentNode.left) queue.push(currentNode.left);
-      if (currentNode.right) queue.push(currentNode.right);
-    }
-    if (queue.length) {
-      handleTree();
-    }
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+  let temp = null,
+    pre = null,
+    cur = head;
+  while (cur) {
+    temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
   }
-  queue.push(root);
-  handleTree();
-  return result;
+  // temp = cur = null;
+  return pre;
 };
 
-// 中序遍历
-
-const tree = {
-  value: "A",
-  left: {
-    value: "B",
-    left: null,
-    right: {
-      value: "D",
-      left: {
-        value: "F",
-        left: null,
-        right: null,
-      },
-      right: null,
-    },
-  },
-  right: {
-    value: "C",
-    left: null,
-    right: {
-      value: "E",
-      left: {
-        value: "G",
-        left: {
-          value: "H",
-          left: null,
-          right: null,
-        },
-        right: {
-          value: "I",
-          left: null,
-          right: null,
-        },
-      },
-    },
-  },
-};
-
-console.log(levelOrder(tree));
+console.log(reverseList(linkedList));
