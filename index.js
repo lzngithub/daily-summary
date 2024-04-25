1deaf978b429698aeb49cd54019451d85ad43a49
liangzn <1763466626@qq.com>
2024-04-26 00:03:30 +0800

    feat: feat

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 let date = new Date(); // 获取当前日期
-// let date = new Date('2022-08-01'); // 生成 2022-08-01 00:00:00:00 这个时间点的时间对象
 
-// 获取年份 月份 日份 星期几
-let year = date.getFullYear(); // getYear(): 返回的是1900至今的年份，比如 2024 返回124
-let month = date.getMonth();
-let day = date.getDate();
-let week = date.getDay();
+// 格式化时间：年-月-日
+let formatDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 
-// 获取时 分 秒 毫秒
-let hour = date.getHours();
-let minute = date.getMinutes();
-let second = date.getSeconds();
-let millisecond = date.getMilliseconds();
+// 格式化星期
+let week = "星期" + "日一二三四五六".charAt(date.getDay());
 
-// 修改时间对象
+// 比较时间大小
+let compareTime = new Date().getTime() - new Date("2010-11-11").getTime();
 
-// 获取时间戳
-let time = date.getTime();
-let time1 = Date.now();
-let time2 = Date.parse(date);
-let time3 = date.valueOf(); // 本质是转为数字: +date 或者 Number(date)
-
-console.log(time2, time, time1);
+console.log(compareTime);
